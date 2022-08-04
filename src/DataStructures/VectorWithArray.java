b755b933065d793244ea00327a86a77e7ed126d3
Onuchukwu Adebayo <adebayop.o@gmail.com>
2022-08-04 01:59:52 +0100

    feat: setup inset method on the vector

--- src/DataStructures/VectorWithArray.java
+++ src/DataStructures/VectorWithArray.java
@@ -1,16 +1,19 @@
 package DataStructures;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class VectorWithArray<T> {
     private int size;
     private int capacity;
     private T[] vector;
-    private final static int INCREMENT_FACTOR = 16;
+    private final static int INCREMENT_FACTOR = 1;
+    private Class<T> vectorClass;
 
     public VectorWithArray(Class<T> vectorClass) {
         this.size = 0;
         this.capacity = INCREMENT_FACTOR;
+        this.vectorClass = vectorClass;
         this.vector = (T[]) Array.newInstance(vectorClass, INCREMENT_FACTOR);
     }
 
@@ -18,6 +21,7 @@ public class VectorWithArray<T> {
         this.vector = (T[]) Array.newInstance(vectorClass, initialCapacity);
         this.size = 0;
         this.capacity = initialCapacity;
+        this.vectorClass = vectorClass;
     }
 
     public int size(){
@@ -37,7 +41,15 @@ public class VectorWithArray<T> {
         return vector[index];
     }
 
-    public void insert(int index, int item){
+    public void insert(int index, T item){
+        if (this.size == this.capacity) {
+            T[] tempVector = (T[]) Array.newInstance(this.vectorClass, 2 * this.capacity);
+            System.arraycopy(vector, 0, tempVector, 0, vector.length);
 
+            this.capacity *= 2;
+            this.vector = tempVector;
+        }
+        this.vector[index] = item;
+        this.size += 1;
     }
 }
